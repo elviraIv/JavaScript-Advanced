191ef6073d9b007a371f97ee8e521708702dd54d
Elvira <elvira.n.ivanova@gmail.com>
2023-05-13 20:59:22 +0300

    create printEveryN-thElement.js

---
+++ arrays-and-nested-arrays/Exercises/printEveryN-thElement.js
@@ -0,0 +1,22 @@
+function print(array, step){
+
+    for (let index = 0; index < array.length; index+=step){
+
+    
+        console.log(array[index]);
+        
+    }
+}
+
+print(['5',
+'20',
+'31',
+'4',
+'20'],
+2
+);
+print(['dsa',
+'asd',
+'test',
+'tset'], 2
+);
