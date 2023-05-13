07e5a89499b0a52197c244134bcfc38afd6b7e4c
Elvira <elvira.n.ivanova@gmail.com>
2023-05-13 18:04:07 +0300

    create printAnArrayWithADelimeter.js

---
+++ arrays-and-nested-arrays/Exercises/printAnArrayWithADelimeter.js
@@ -0,0 +1,19 @@
+function print(array,delimeter){
+
+    
+    console.log(array.join(delimeter));
+}
+
+print(['One',
+'Two',
+'Three',
+'Four',
+'Five'],
+'-'
+);
+print(['How about no?', 'I',
+'will',
+'not',
+'do', 'it!'],
+'_'
+)
