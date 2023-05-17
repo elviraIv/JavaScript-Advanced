4543596180e9e50c20eab1086f501850591e349d
Elvira <elvira.n.ivanova@gmail.com>
2023-05-17 22:45:37 +0300

    create assembly-line.js

---
+++ objects-and-composition/Lab/assembly-line.js
@@ -0,0 +1,68 @@
+function createAssemblyLine(){
+    return{
+        hasClima(object){
+            object.temp = 21;
+            object.tempSettings = 21;
+            object.adjustTemp = () =>{
+                if(object.temp < object.tempSettings){
+                    object.temp++;
+                }else if(object.temp > object.tempSettings){
+                    object.temp--;
+                }
+            }
+        },
+        hasAudio(object){
+                object.currentTrack = null;
+                object.nowPlaying = () =>{
+                console.log(`Now playing '${object.currentTrack.name}' by ${object.currentTrack.artist}`);
+                };
+            },
+        hasParktronic(object){
+                object.checkDistance = (distance) =>{
+                    let sound = '';
+                    if (distance < 0.1) {
+                        sound = 'Beep! Beep! Beep!';
+                      } else if (distance >= 0.1 && distance < 0.25) {
+                        sound = 'Beep! Beep!';
+                      } else if (distance >= 0.25 && distance < 0.5) {
+                        sound = 'Beep!';
+                      }
+                      console.log(sound);
+
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+const assemblyLine = createAssemblyLine();
+
+const myCar = { make: "Toyota", model: "Avensis" };
+
+assemblyLine.hasClima(myCar);
+console.log(myCar.temp);
+myCar.tempSettings = 18;
+myCar.adjustTemp();
+console.log(myCar.temp);
+
+assemblyLine.hasAudio(myCar);
+myCar.currentTrack = {
+  name: "Never Gonna Give You Up",
+  artist: "Rick Astley",
+};
+myCar.nowPlaying();
+
+assemblyLine.hasParktronic(myCar);
+myCar.checkDistance(0.4);
+myCar.checkDistance(0.2);
+
+console.log(myCar);
