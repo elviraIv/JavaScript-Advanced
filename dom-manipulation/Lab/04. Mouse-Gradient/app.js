029e8cc497b7b59bf97a4265ceb587feaf3bf441
Elvira <elvira.n.ivanova@gmail.com>
2023-05-30 19:52:04 +0300

    modiefied Mouse-Gradient/app.js

--- dom-manipulation/Lab/04. Mouse-Gradient/app.js
+++ dom-manipulation/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,11 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientElement = document.getElementById('gradient');
+    let resultElement = document.getElementById('result');
+
+    gradientElement.addEventListener('mousemove', show);
+    function show(e){
+        let percent = Math.floor((e.offsetX / e.target.offsetWidth) * 100); 
+        resultElement.textContent = percent +'%';
+    }
+
 }
