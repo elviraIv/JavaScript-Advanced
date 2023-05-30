5a0adbe3b6fdb8949889409f236c8aeedf66004b
Elvira <elvira.n.ivanova@gmail.com>
2023-05-30 20:56:46 +0300

    modified Dynamic-Validation/app.js

--- dom-manipulation/Lab/06. Dynamic-Validation/app.js
+++ dom-manipulation/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    console.log('TODO:...');
+    let emailInputElement = document.getElementById('email');
+    let pattern = /[a-z]+@[a-z]+\.[a-z]+/gm;
+
+    emailInputElement.addEventListener('change', onchange);
+
+    function onchange(){
+        if(pattern.test(emailInputElement.value)){
+            emailInputElement.classList.remove('error')
+        }else{
+            emailInputElement.classList.add('error')
+        }
+    }
+
+
 }
