cf995f838ea95e1a5c1ff83ad4cd05c32eec8c57
Elvira <elvira.n.ivanova@gmail.com>
2023-05-30 20:26:53 +0300

    modified Highlight-Active/app.js

--- dom-manipulation/Lab/05. Highlight-Active/app.js
+++ dom-manipulation/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focused() {
-    console.log('TODO:...');
+    let divElements = document.querySelectorAll('input');
+    Array.from(divElements).forEach(x =>{
+        x.addEventListener('focus', onfocus);
+        x.addEventListener('blur', onblur);
+    });
+
+    function onfocus(e){
+        e.target.parentElement.classList.add('focused');
+    }
+
+    function onblur(e){
+        e.target.parentElement.classList.remove('focused')
+    }
+   
 }
