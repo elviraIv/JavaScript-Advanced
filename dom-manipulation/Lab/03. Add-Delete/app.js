d23158e7bd9f437af4d71a98227b0465b31a480f
Elvira <elvira.n.ivanova@gmail.com>
2023-05-30 19:20:35 +0300

    modified

--- dom-manipulation/Lab/03. Add-Delete/app.js
+++ dom-manipulation/Lab/03. Add-Delete/app.js
@@ -1,3 +1,24 @@
 function addItem() {
-    //TODO...
+    let inputElement = document.getElementById('newItemText');
+    let newLi = document.createElement('li');
+    let ulElement = document.getElementById('items');
+    newLi.textContent = inputElement.value;
+
+    ulElement.appendChild(newLi);
+    inputElement.value='';
+
+//create the delete btn
+    let deleteBtn = document.createElement('a');
+    deleteBtn.textContent = '[Delete]';
+    deleteBtn.href = '#';
+    newLi.appendChild(deleteBtn);
+
+//add event when delete btn is clicked
+    deleteBtn.addEventListener('click', deleteElement);
+    function deleteElement(e){
+        e.currentTarget.parentNode.remove()
+    }
+
+
+    
 }
